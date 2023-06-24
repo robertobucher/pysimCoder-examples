1d09dbef6ce0aa7603996ea9d647a6beab4c6ced
Pavel Pisa <ppisa@pikron.com>
2023-06-25 01:51:40 +0200

    linux_mz_apo: DC motor follower with tunable PID controller updated
    The example updated to the new pysimCoder format and TCP port
    for RT scope updated to use TCPsocketAsync block which
    does not include blocking in the control thread.
    
    Signed-off-by: Pavel Pisa <ppisa@pikron.com>

--- Linux-mzapo/DCmotor/DC_PID_follower-run.sh
+++ Linux-mzapo/DCmotor/DC_PID_follower-run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-TARGET=192.168.136.86
-HOST=192.168.136.183
+TARGET=192.168.1.37
+HOST=192.168.1.10
+SHV_BROKER=192.168.1.10:3755
 
 scp DC_PID_follower root@$TARGET:/tmp
-ssh -t root@$TARGET HOST=$HOST /tmp/DC_PID_follower
+ssh -t "root@$TARGET" HOST="$HOST" /tmp/DC_PID_follower -D SHV_BROKER="$SHV_BROKER"
+
+#nc -l -p 1024 | hexdump -e '2/8 "%10f " 1/8 "%10f\n"'
